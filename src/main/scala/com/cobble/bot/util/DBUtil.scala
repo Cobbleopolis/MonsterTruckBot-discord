741040e3ee6d1802a07e89f90e8f3ceb9651c477
Cobbleopolis <cobbleopolis@gmail.com>
2017-03-02 19:54:45 -0500

    Updated Scaladoc

--- src/main/scala/com/cobble/bot/util/DBUtil.scala
+++ src/main/scala/com/cobble/bot/util/DBUtil.scala
@@ -9,13 +9,16 @@ import scala.concurrent.Future
 
 object DBUtil {
 
+    /**
+      * Database using the provided config.
+      */
     val db: _root_.slick.jdbc.PostgresProfile.backend.DatabaseDef = Database.forConfig("mtrBotDB")
 
     /**
-      * Batch inserts [[BotInstance]]s.
+      * Batch inserts [[com.cobble.bot.db.Tables#BotInstance]]s.
       *
-      * @param botInstances The [[BotInstance]]s to insert.
-      * @return A [[Future]] that inserts the passed [[BotInstance]]s.
+      * @param botInstances The [[com.cobble.bot.db.Tables#BotInstance]]s to insert.
+      * @return A [[scala.concurrent.Future]] that inserts the passed [[com.cobble.bot.db.Tables#BotInstance]]s.
       */
     def insertBotInstances(botInstances: BotInstance*): Future[Option[Int]] = {
         db.run(
@@ -23,12 +26,25 @@ object DBUtil {
         )
     }
 
+    /**
+      * Updates a guild's moderator role
+      *
+      * @param guildId       The id of the guild to update the moderator role for.
+      * @param moderatorRole The new moderator role for the guild.
+      * @return A [[scala.concurrent.Future]] that returns an int of how many rows were updating. Should either be `1` or `0`.
+      */
     def setGuildModeratorRole(guildId: String, moderatorRole: IRole): Future[Int] = {
         db.run(
             BotInstancesRef.updateModRoleId(guildId).update(Some(moderatorRole.getID))
         )
     }
 
+    /**
+      * Gets a guild from its id.
+      *
+      * @param guildId The id of the guild to get.
+      * @return A [[scala.concurrent.Future]] that optionally returns an [[com.cobble.bot.db.Tables#BotInstance]] that has the provided id. If the provided id does not match any entries [[scala.None]] is returned.
+      */
     def getBotInstanceById(guildId: String): Future[Option[BotInstance]] = {
         db.run(
             BotInstancesRef.getById(guildId).result.headOption
--- src/main/scala/com/cobble/bot/util/MessageUtil.scala
+++ src/main/scala/com/cobble/bot/util/MessageUtil.scala
@@ -4,13 +4,39 @@ import com.cobble.bot.CommandRegistry
 
 object MessageUtil {
 
+    /**
+      * The arrow character used when responding to a command.
+      */
     val arrowChar: Char = '\u21D2'
 
-    def formatMessage(userMention: String, message: String): String = s"$userMention $arrowChar $message"
-
+    /**
+      * Formats an error message mentioning a user.
+      *
+      * @param userMention A string used to mention a user.
+      * @param message     The content of the message specific to what happened.
+      * @param throwable   The error that was thrown.
+      * @return The formatted error message.
+      */
     def getErrorMessage(userMention: String, message: String, throwable: Throwable): String =
         formatMessage(userMention, s"$message \n\nPlease report this message to the bot developer as well as the command you used that returned this error.\n```${throwable.getMessage}```")
 
+    /**
+      * Properly formats a response message mentioning a user.
+      *
+      * @param userMention A string used to mention a user.
+      * @param message     The content of the message to format.
+      * @return The formatted message.
+      */
+    def formatMessage(userMention: String, message: String): String = s"$userMention $arrowChar $message"
+
+    /**
+      * Formats a message in response to a user that incorrectly used a command.
+      *
+      * @param userMention A string used to mention a user.
+      * @param message     Text describing how the command was misused.
+      * @param name        The name of the misused command.
+      * @return The formatted usage error message.
+      */
     def commandUsageError(userMention: String, message: String, name: String): String = formatMessage(userMention, s"$message Please use `!${CommandRegistry.helpCommand.name} $name` to see how to use this command.")
 
 }
