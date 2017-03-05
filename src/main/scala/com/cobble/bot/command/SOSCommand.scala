e9d2109dc7bfeb3388edf872a3b4133d063be90d
Cobbleopolis <cobbleopolis@gmail.com>
2017-03-04 23:00:19 -0500

    The SOS command now deletes the calling message and sends a sent receipt to the author.

--- src/main/scala/com/cobble/bot/command/SOSCommand.scala
+++ src/main/scala/com/cobble/bot/command/SOSCommand.scala
@@ -2,7 +2,7 @@ package com.cobble.bot.command
 
 import com.cobble.bot.MonsterTruckBot
 import com.cobble.bot.util.{DBUtil, MessageUtil}
-import sx.blah.discord.handle.obj.{IGuild, IMessage}
+import sx.blah.discord.handle.obj.{IGuild, IMessage, IUser}
 
 import scala.collection.JavaConverters._
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -29,8 +29,10 @@ class SOSCommand extends Command {
         if (message.getChannel.isPrivate)
             message.getChannel.sendMessage("You can not send a SOS from a direct message.")
         else {
+            message.delete()
             val guild: IGuild = message.getGuild
-            val sosTextBuilder: StringBuilder = new StringBuilder(s"${message.getAuthor.mention} just sent an SOS from ${message.getChannel.mention}.")
+            val author: IUser = message.getAuthor
+            val sosTextBuilder: StringBuilder = new StringBuilder(s"${author.mention} just sent an SOS from ${message.getChannel.mention}.")
             if (args.nonEmpty)
                 sosTextBuilder.append(s"\n\t**SOS Message:** ${args.mkString(" ")}")
             val sosText: String = sosTextBuilder.toString
@@ -38,16 +40,17 @@ class SOSCommand extends Command {
                 case Success(maybeBotInstance) =>
                     if (maybeBotInstance.isDefined) {
                         val maybeModeratorRoleId: Option[String] = maybeBotInstance.get.moderatorRoleId
-                        if (maybeModeratorRoleId.isDefined)
+                        if (maybeModeratorRoleId.isDefined) {
                             guild.getUsersByRole(guild.getRoleByID(maybeModeratorRoleId.get)).asScala.filterNot(_.isBot).foreach(user => {
                                 MonsterTruckBot.client.getOrCreatePMChannel(user).sendMessage(sosText)
                             })
-                        else
-                            message.getAuthor.getOrCreatePMChannel().sendMessage("No moderator role is set for this server. Please ask your mods to set one.")
+                            author.getOrCreatePMChannel.sendMessage("SOS successfully sent to the moderators.")
+                        } else
+                            author.getOrCreatePMChannel.sendMessage("No moderator role is set for this server. Please ask your mods to set one.")
                     } else
-                        message.getChannel.sendMessage(MessageUtil.formatMessage(message.getAuthor.mention, " This server is not found in the database. I don't know how you called this command..."))
+                        author.getOrCreatePMChannel.sendMessage(MessageUtil.formatMessage(author.mention, " This server is not found in the database. I don't know how you called this command..."))
                 case Failure(t) =>
-                    message.getChannel.sendMessage(MessageUtil.getErrorMessage(message.getAuthor.mention, "There was an issue sending the SOS.", t))
+                    author.getOrCreatePMChannel.sendMessage(MessageUtil.getErrorMessage(author.mention, "There was an issue sending the SOS.", t))
             }
         }
     }
