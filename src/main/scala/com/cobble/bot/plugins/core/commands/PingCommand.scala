10ba38aba98d4834a6ea391e9a1aec90cb328322
Cobbleopolis <cobbleopolis@gmail.com>
2017-03-09 23:33:01 -0500

    Updated ping command to use message formatting

--- src/main/scala/com/cobble/bot/plugins/core/commands/PingCommand.scala
+++ src/main/scala/com/cobble/bot/plugins/core/commands/PingCommand.scala
@@ -1,10 +1,8 @@
 package com.cobble.bot.plugins.core.commands
 
 import com.cobble.bot.api.Command
-import sx.blah.discord.handle.obj.{IMessage, IUser}
-
-import scala.collection.JavaConverters._
-import scala.collection.mutable
+import com.cobble.bot.util.MessageUtil
+import sx.blah.discord.handle.obj.IMessage
 
 /**
   * The PingCommand class, this implements the [[com.cobble.bot.api.Command]] trait.
@@ -17,18 +15,14 @@ class PingCommand extends Command {
 
     override val name: String = "ping"
 
-    override val usageText: String = "[username_mentions...]"
+    override val usageText: String = ""
 
     override val helpText: String = "A simple command to ping the bot to make sure it is alive."
 
     override val briefHelpText: String = helpText
 
     override def execute(message: IMessage, args: Array[String]): Unit = {
-        val mentions: mutable.Buffer[IUser] = message.getMentions.asScala
-        if (mentions.nonEmpty)
-            mentions.foreach(user => message.getChannel.sendMessage(s"pong ${user.mention}"))
-        else
-            message.getChannel.sendMessage(s"pong!")
+        message.getChannel.sendMessage(MessageUtil.formatMessage(message.getAuthor.mention, "pong!"))
     }
 
 }
